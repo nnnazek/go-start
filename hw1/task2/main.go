41ce55a8c127cee2eee3eacfd39c399ab6d241d7
Nazerke Zhansugirova <nazek@Nazerkes-MacBook-Air.local>
2023-09-15 03:15:10 +0600

    Homework

--- hw1/task2/main.go
+++ hw1/task2/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+	"os"
+)
 
+func main() {
+	var radius float64
+
+	fmt.Print("Print radius: ")
+
+	fmt.Fscan(os.Stdin, &radius)
+
+	area := math.Pi * math.Pow(radius, 2)
+
+	circumference := 2 * math.Pi * radius
+
+	fmt.Printf("Area of circle: %.2f\n", area)
+	fmt.Printf("Circumference: %.2f\n", circumference)
 }
